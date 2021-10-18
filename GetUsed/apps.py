297121ace4d7b75d4b20cf190e8b40e34e52caf3
Tobu-cloud9 <kanmi132255817@gmail.com>
2021-10-18 20:04:34 +0900

    runserver-helloworld

---
+++ GetUsed/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GetUsedConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GetUsed'
---
+++ pro_django/local_settings.py
@@ -0,0 +1,5 @@
+SECRET_KEY_LS = 'django-insecure-3z*#vo6w1fz01#(&0&eyhv6(f5)^s-vugxksbww*ebg%$*nmy_'
+DB_NAME = 'items'
+DB_USER = 'items_user'
+DB_PASSWORD = 'django0000'
+DB_ROOT_PASSWORD = 'django0001'
